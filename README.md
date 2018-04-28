e899f38949ce40809a8ee6b3e973b2ca5b5310b2
unknown <Tech@C416IT29.gbc.local>
2018-04-28 14:06:50 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # charge
-GBCJam 12
+GBCJam 12 - Game Jam - Make a game in 12 hours
+
+Authors:
+Andrew Oatley-Willis
+Billy Tremaine
+Christoph Schoch
+
+Music By:
+
+Game Engine:
+phaser.io
--- game.js
+++ game.js
@@ -22,21 +22,28 @@ var platforms;
 var player;
 var score = 0;
 var scoreText;
+var bomb;
+
 
 function preload () {
     // Charge assets
     this.load.image('bboy_pos_default', 'assets/bboy_default.png');
     this.load.image('bboy_neg_default', 'assets/bboy_neg_default.png');
+    this.load.spritesheet('bboy_walk',
+        'assets/bboy_walk.png',
+        { frameWidth: 64, frameHeight: 64 }
+    );
+
 
     // Tutorial assets
     this.load.image('sky', 'tutorial/assets/sky.png');
     this.load.image('ground', 'tutorial/assets/platform.png');
     this.load.image('star', 'tutorial/assets/star.png');
     this.load.image('bomb', 'tutorial/assets/bomb.png');
-    this.load.spritesheet('dude',
+    /*this.load.spritesheet('dude',
         'tutorial/assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
-    );
+    );*/
 }
 
 
@@ -53,25 +60,28 @@ function create () {
     platforms.create(750, 220, 'ground');
 
     // Player
-    player = this.physics.add.sprite(100, 450, 'dude');
+    player = this.physics.add.sprite(100, 450, 'bboy_pos_default');
     player.setBounce(0.15);
     player.setCollideWorldBounds(true);
+
+    // Bombs
+    bombs = this.physics.add.group();
 
     // Animations
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('bboy_walk', { start: 0, end: 4 }),
         frameRate: 10,
         repeat: -1
     });
     this.anims.create({
         key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
+        frames: [ { key: 'bboy_walk', frame: 0 } ],
         frameRate: 20
     });
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('bboy_walk', { start: 0, end: 4 }),
         frameRate: 10,
         repeat: -1
     });
@@ -91,19 +101,52 @@ function create () {
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
     this.physics.add.overlap(player, stars, collectStar, null, this);
+    this.physics.add.collider(bombs, platforms);
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
+
 
     // Enable keyboard bindings
     cursors = this.input.keyboard.createCursorKeys();
 
+    // Add text information
+    scoreText = this.add.text(16, 16, 'score: 0' , { fontSize: '32px', fill: '#000' });
+
 }
 
 // Function to run on star player collision
-function collectStar (player, star) {
+function collectStar (player, star)
+{
     star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0) {
+        stars.children.iterate(function (child) {
+            child.enableBody(true, child.x, 0, true, true);
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+
+    }
+}
+
+// Function to run on bomb player collision
+function hitBomb (player, bomb) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+    gameOver = true;
 }
 
 // Update function controls keyboard input
 function update () {
+
     if (cursors.left.isDown) {
         player.setVelocityX(-160);
         player.anims.play('left', true);
