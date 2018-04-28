5871f4939c949e7efe92e83e817986e47ac01cf6
unknown <Tech@C416IT29.gbc.local>
2018-04-28 14:12:38 -0400

    Add placeholder images

--- game.js
+++ game.js
@@ -36,10 +36,10 @@ function preload () {
 
 
     // Tutorial assets
-    this.load.image('sky', 'tutorial/assets/sky.png');
-    this.load.image('ground', 'tutorial/assets/platform.png');
-    this.load.image('star', 'tutorial/assets/star.png');
-    this.load.image('bomb', 'tutorial/assets/bomb.png');
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
     /*this.load.spritesheet('dude',
         'tutorial/assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
