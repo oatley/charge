e46a1b2bcf39e060a38120c73f1252639358f37f
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-04-30 16:17:20 -0400

    No flying agian

--- game.js
+++ game.js
@@ -13,8 +13,8 @@ function preload() {
 var playerDirection = 'left';
 var playerJumpSensitivity = -5; // Negative number, default is about -2.8 falling always
 var playerMoveSpeed = 250;
-var playerJumpSpeed = -100; // Negative number
-var playerJumpSpeedMod = -300;
+var playerJumpSpeed = -400; // Negative number
+var playerJumpSpeedMod = -100;
 var playerJumping = false;
 var playerJumpMaxHeight = -400;
 
@@ -142,9 +142,11 @@ function update() {
     }
 
     //  Allow the player to jump if they are touching the ground.
-    if (!cursors.up.isDown) {
+    if (!cursors.up.isDown && playerJumping) {
         player.body.velocity.y = 0;
+        playerJumping = false;
     } else if (cursors.up.isDown && player.body.touching.down) {
         player.body.velocity.y = playerJumpSpeed;
+        playerJumping = true;
     }
 }
