cb8f31f9eb082bf17a3170f5a5ba26cfd335add7
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-04-30 20:02:04 -0400

    New upPositiveButton

--- game.js
+++ game.js
@@ -115,7 +115,7 @@ function create() {
     playerNegativeGroup = game.add.group();
     for (var i = 0; i < 3; i++) {
         // The player and its settings
-        p = game.add.sprite(game.world.width -128 * i, game.world.height - 150, 'battery-neg');
+        p = game.add.sprite(game.world.width -128 * i, game.world.height - 200, 'battery-neg');
         configurePlayer(p, playerNegativeGroup); // Default settings + group
     }
 
